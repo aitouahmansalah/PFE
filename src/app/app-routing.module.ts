36d021eb2bc0f737b9edb585757658d9d7efc510
aitouahmansalah <salahaitouahman1@gmail.com>
2024-02-17 16:16:39 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'barcode-scanning',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: 'barcode-scanning',
+    loadChildren: () =>
+      import('./modules/barcode-scanning/barcode-scanning.module').then(
+        (m) => m.BarcodeScanningModule,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'barcode-scanning',
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
